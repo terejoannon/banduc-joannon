708401ab69131b54fc6c012478525d437eafdccc
terejoannon <54028448+terejoannon@users.noreply.github.com>
2019-08-25 16:32:40 -0300

    Add files via upload

--- portafolio.js
+++ portafolio.js
@@ -1,43 +1,87 @@
 var trabajos = [{
-    figure: "media/trabajo-01-desfile.jpg",
+    figure: "media/trabajo-01-desfile.png",
     alt: "Descripción de la imagen",
-    title: "Título de primer trabajo",
-    about: "Texto simulado de descripción de primer trabajo"
+    title: "MUP", 
+    about: "Examen final Taller Identidad"
 }, {
-    figure: "media/trabajo-02-app.jpg",
+    figure: "media/trabajo-02-app.png",
     alt: "Descripción de la imagen",
-    title: "Título de segundo trabajo",
-    about: "Texto simulado de descripción de segundo trabajo"
+    title: "Desarrollo de App movil",
+    about: "Aplicación movil en desarrollo"
 }, {
-    figure: "media/trabajo-03-homy.jpg",
+    figure: "media/trabajo-03-homy.png",
     alt: "Descripción de la imagen",
-    title: "Título de tercer trabajo",
-    about: "Texto simulado de descripción de tercer trabajo"
+    title: "PAD",
+    about: "Examen final Taller Producto 2018"
 }, {
-    figure: "media/trabajo-04-editorial.jpg",
+    figure: "media/OxxoFlex.png",
     alt: "Descripción de la imagen",
-    title: "Título de cuarto trabajo",
-    about: "Texto simulado de descripción de cuarto trabajo"
+    title: "OXXOFLEX",
+    about: "Mobiliario para campus univestario Lo Contador"
 }, {
-    figure: "media/trabajo-05-artesania.jpg",
+    figure: "media/Gretsch.png",
     alt: "Descripción de la imagen",
-    title: "Título de quinto trabajo",
-    about: "Texto simulado de descripción de quinto trabajo"
+    title: "Gretsch",
+    about: "Modelado 3D guitarra electrica"
 }, {
-    figure: "media/trabajo-06-textil.jpg",
+    figure: "media/Pass.png",
     alt: "Descripción de la imagen",
-    title: "Título de sexto trabajo",
-    about: "Texto simulado de descripción de sexto trabajo"
+    title: "Pass",
+    about: "Insumo profesional de gastronomia"
 }, {
-    figure: "media/trabajo-07-materiales.jpg",
+    figure: "media/Merengoza 1.png",
     alt: "Descripción de la imagen",
-    title: "Título de séptimo trabajo",
-    about: "Texto simulado de descripción de séptimo trabajo"
+    title: "Merengoza",
+    about: "Desarrollo de productos gastronomicos identitarios"
+}, {
+    figure: "media/Jabon.png",
+    alt: "Descripción de la imagen",
+    title: "Moon",
+    about: "Experimentación de propiedades fisicas de la parafina sólida"
+
+}, {
+    figure: "media/YesoElastico.png",
+    alt: "Descripción de la imagen",
+    title: "Yeso Elastico",
+    about: "Exploración de nuevas propiedades del material del yeso"
+}, {
+    figure: "media/Cabless.png",
+    alt: "Descripción de la imagen",
+    title: "Cabless",
+    about: "Proyecto de Gestion de diseño para produccion industrial"
+}, {
+    figure: "media/Ilko.png",
+    alt: "Descripción de la imagen",
+    title: "Campaña 50 años",
+    about: "Proyecto ilko/virutex, 50 años del ostador estañado"
+}, {
+    figure: "media/Easy.png",
+    alt: "Descripción de la imagen",
+    title: "Easy iron",
+    about: "Proyecto enfocado en reducir la domesticidad, plancha sin plancha"
+}, {
+    figure: "media/Muriel.png",
+    alt: "Descripción de la imagen",
+    title: "Muriel",
+    about: "Mobiliario experimental para campus universitario"
+}, {
+    figure: "media/Guaca.png",
+    alt: "Descripción de la imagen",
+    title: "Comedero Zoonanimal",
+    about: "Comedero para guacamayos del zoologico metropolitano"
+}, {
+    figure: "media/Grik.gif",
+    alt: "Descripción de la imagen",
+    title: "Grik",
+    about: "Exploración en identidad gastronomica"
+
+
 }];
-// LUEGO CREAMOS PARTES DEL SITIO CON CADA DATO CONTENIDO EN LA VARIABLE
+/* LUEGO CREAMOS PARTES DEL SITIO CON CADA DATO CONTENIDO EN LA VARIABLE */
 $.each(trabajos, function(i, val) {
     var uno = '<figure><img src="' + trabajos[i].figure + '" alt="' + trabajos[i].alt + '"/></figure>';
     var dos = '<h4>' + trabajos[i].title + '</h4>';
     var tres = '<p>' + trabajos[i].about + '</p>';
-    $('#portafolio').append('<div class="item">' + uno + dos + tres + '</div>')
+    var cuatro = '<p><a href= "https://drive.google.com/drive/folders/1mn9eFkvHURN8-dgMArkq_Xx8LsXPiJOg">Ver más</a><p>'
+    $('#portafolio').append('<div class="item">' + uno + dos + tres + cuatro + '</div>')
 });
